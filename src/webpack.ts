b4086f6ca11a89e3958edb02f9b533f97ade502a
SepVeneto <sepveneto@qq.com>
2022-12-14 22:22:50 +0800

    feat: webpack loader

--- src/webpack.ts
+++ src/webpack.ts
@@ -1,1 +1,18 @@
-export default function(code: string)
+import type * as webpack from 'webpack'
+import { addToFooter, addToHeader, genSlotCode, getPages, getTemplate, isAppVue, isEntryPage } from './util'
+let header: string[] = []
+let footer: string [] = []
+const entryList = getPages()
+export default function (this: webpack.LoaderContext<{}>, code: string) {
+  if (isAppVue(this.resourcePath)) {
+    const { origin, tpl } = getTemplate(code)
+    const [_header, _footer] = genSlotCode(tpl)
+    header = _header
+    footer = _footer
+    return origin
+  } else if (isEntryPage(this.resourcePath, entryList)) {
+    code = addToHeader(code, header)
+    code = addToFooter(code, footer)
+  }
+  return code
+}
