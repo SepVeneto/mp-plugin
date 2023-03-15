d8bca5a3d7b94a3b04d02e3915cae7b2653fd032
SepVeneto <sepveneto@qq.com>
2023-03-15 16:49:05 +0800

    fix: collect兼容webpack

--- src/webpack.ts
+++ src/webpack.ts
@@ -1,9 +1,9 @@
 import type * as webpack from 'webpack'
 import type { Options } from './types'
 import { getPages, transform } from './util'
-const entryPages = getPages()
 export default function (this: webpack.LoaderContext<Options>, code: string) {
   // webpack4 降级
-  const { log = 'error' } = this.getOptions?.() ?? this.query
+  const { log = 'error', collect = true } = this.getOptions?.() ?? this.query
+  const entryPages = getPages(collect)
   return transform(this.resourcePath, code, entryPages, log)
 }
