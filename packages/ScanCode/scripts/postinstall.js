35fa27f26ed4df5f21c81b01f9299d16fff2e4e4
SepVeneto <sepveneto@qq.com>
2024-10-06 14:53:57 +0800

    fix(scan-code): local install

--- packages/ScanCode/scripts/postinstall.js
+++ packages/ScanCode/scripts/postinstall.js
@@ -7,6 +7,8 @@ const depsRoot = path.resolve(packageRoot, '../..')
 
 const wasm = path.resolve(depsRoot, 'zxing-wasm/dist/full/zxing_full.wasm')
 const target = path.resolve(process.env.INIT_CWD, 'public/zxing_full.wasm')
-fs.copyFileSync(wasm, target)
+if (fs.existsSync(wasm)) {
+  fs.copyFileSync(wasm, target)
+}
 
 // throw new Error(JSON.stringify(process.env, null, 2))
