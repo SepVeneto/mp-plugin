7dc452f9f55310d2a623789c09e9de3eeae34485
SepVeneto <sepveneto@qq.com>
2024-10-08 16:08:16 +0800

    feat(scan-code): use worker

--- packages/ScanCode/lib/ScanCode.vue
+++ packages/ScanCode/lib/ScanCode.vue
@@ -33,6 +33,44 @@ import {
 let moveOffset = 100
 const SPEED = 100
 const HEIGHT = 10
+let pts = 0
+let checkCount = 0
+
+const workerScript = `
+self.importScripts('https://cdn.jsdelivr.net/npm/zxing-wasm@1.2.12/dist/iife/full/index.js')
+console.log('worker start')
+
+ZXingWASM.getZXingModule({
+  locateFile: (path, prefix) => {
+    if (path.endsWith('.wasm')) {
+      const url = self.location.origin + '/' + path
+      return url
+    }
+    return prefix + path
+  }
+})
+
+self.onmessage = (evt) => {
+  const { type, data } = evt.data
+  switch (type) {
+    case 'scan':
+      ZXingWASM.readBarcodesFromImageData(data, { formats: []}).then(res => {
+        if (res.length === 0) {
+          return
+        }
+        self.postMessage({ type: 'success', data: res })
+      })
+      break
+    case 'file':
+      ZXingWASM.readBarcodesFromImageFile(data).then(res => {
+        if (res.length === 0) {
+          return
+        }
+        self.postMessage({ type: 'success', data: res })
+      })
+  }
+}
+`
 
 const IconClose = 'data:image/svg+xml;charset=utf-8;base64,PHN2ZyB0PSIxNzI4MzQ5MzIwNjIyIiBjbGFzcz0iaWNvbiIgdmlld0JveD0iMCAwIDEwMjQgMTAyNCIgdmVyc2lvbj0iMS4xIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHAtaWQ9IjU4NjUiIHdpZHRoPSIyMDAiIGhlaWdodD0iMjAwIj48cGF0aCBkPSJNNTEyIDQ1MS42NjkzMzNMNDIxLjUwNCAzNjEuMTMwNjY3QTQyLjY2NjY2NyA0Mi42NjY2NjcgMCAwIDAgMzYxLjEzMDY2NyA0MjEuNTQ2NjY3TDQ1MS42NjkzMzMgNTEybC05MC41Mzg2NjYgOTAuNDk2YTQyLjY2NjY2NyA0Mi42NjY2NjcgMCAwIDAgNjAuMzczMzMzIDYwLjM3MzMzM0w1MTIgNTcyLjMzMDY2N2w5MC40OTYgOTAuNTM4NjY2YTQyLjY2NjY2NyA0Mi42NjY2NjcgMCAwIDAgNjAuMzczMzMzLTYwLjM3MzMzM0w1NzIuMzMwNjY3IDUxMmw5MC41Mzg2NjYtOTAuNDk2YTQyLjY2NjY2NyA0Mi42NjY2NjcgMCAxIDAtNjAuMzczMzMzLTYwLjM3MzMzM0w1MTIgNDUxLjY2OTMzM3ogbS0yNzEuNTMwNjY3IDMzMS44NjEzMzRBMzg0IDM4NCAwIDEgMSA3ODMuNTMwNjY3IDI0MC40NjkzMzMgMzg0IDM4NCAwIDAgMSAyNDAuNDY5MzMzIDc4My41MzA2Njd6IiBmaWxsPSIjYmZiZmJmIiBwLWlkPSI1ODY2Ij48L3BhdGg+PC9zdmc+'
 
@@ -51,26 +89,41 @@ export default {
       videoObj: null,
       event: null,
       frameHandler: null,
+      stats: null,
+      worker: null,
     }
   },
   onLoad(options) {
+    const blob = new Blob([workerScript], { type: 'application/javascript'})
+    this.worker = new Worker(URL.createObjectURL(blob))
+    this.worker.onmessage = (evt) => {
+      const { type, data } = evt.data
+      switch (type) {
+        case 'success':
+          this.emitRes(data[0])
+          break
+      }
+    }
+
     this.normalizeOptions(options)
 
     this.event = this.getOpenerEventChannel();
-    getZXingModule({
-      locateFile: (path, prefix) => {
-        if (path.endsWith('.wasm')) {
-          const url = `${window.location.origin}/${path}`
-          return url
-        }
-        return prefix + path
-      }
-    })
+    // getZXingModule({
+    //   locateFile: (path, prefix) => {
+    //     if (path.endsWith('.wasm')) {
+    //       const url = `${window.location.origin}/${path}`
+    //       return url
+    //     }
+    //     return prefix + path
+    //   }
+    // })
   },
   mounted() {
+    // this.stats = new Stats()
+    // document.body.appendChild(this.stats.dom)
     this.canvasObj = this.$refs.canvasRef
     this.videoObj = this.$refs.videoRef
-    this.ctx = this.canvasObj.getContext('2d')
+    this.ctx = this.canvasObj.getContext('2d', { willReadFrequently: true })
     this.width = window.innerWidth
     this.height = window.innerHeight
     this.canvasObj.width = this.width
@@ -84,7 +137,7 @@ export default {
     this.videoObj.setAttribute('playsinline', true)
     this.videoObj.addEventListener('loadedmetadata', () => {
       this.videoObj.play()
-      this.scaning()
+      window.requestAnimationFrame(this.scaning)
     })
 
     navigator.mediaDevices.getUserMedia({
@@ -121,10 +174,10 @@ export default {
       const offset = moveOffset + (delta ? SPEED * (1 / delta) : 0)
       const lineargradient = this.ctx.createLinearGradient(30, offset, 30, HEIGHT + offset)
       lineargradient.addColorStop(0, 'rgba(122,114,231,0)')
-      lineargradient.addColorStop(1, `rgba(122,114,231,${transition((offset * 2) / (window.innerHeight - 100))})`)
+      lineargradient.addColorStop(1, `rgba(122,114,231,${transition((offset * 2) / (this.height - 100))})`)
       this.ctx.fillStyle = lineargradient
-      this.ctx.fillRect(30, offset, window.innerWidth - 60, HEIGHT)
-      moveOffset = (offset > window.innerHeight - 100) ? 100 : offset
+      this.ctx.fillRect(30, offset, this.width - 60, HEIGHT)
+      moveOffset = (offset > this.height - 100) ? 100 : offset
     },
     drawBounding(location) {
       const ctx = this.ctx
@@ -155,30 +208,47 @@ export default {
       ctx.stroke();
     },
 
-    scaning(startTime = Date.now()) {
-      const current = Date.now()
-      const delta = current - startTime
+    scaning(ts) {
+      this.frameHandler = window.requestAnimationFrame(this.scaning)
+      const delta = ts - pts
+      // if (delta < 1000 / 60) {
+      //   return
+      // }
+
+      // this.stats.begin()
+
 
       const width = this.width
       const height = this.height
+      this.ctx.clearRect(0, 0, width, height)
       this.ctx.drawImage(this.videoObj, 0, 0, width, height)
       const img = this.ctx.getImageData(0, 0, width, height)
 
       this.draw(delta)
+      pts = ts
 
-      readBarcodesFromImageData(img, {
-        formats: this.formats,
-        // TODO: 当一次识别出多个时，允许选择
-        maxNumberOfSymbols: 1,
-      }).then(res => {
-        if (Array.isArray(res) ? res.length === 0 : res.scanType === 'None') return
+      // console.log('real', Math.floor(1000 / delta))
+      if (++checkCount < 30) {
+        return
+      }
+      checkCount = 0
 
-        this.emitRes(res)
-        uni.navigateBack()
-      }).catch(err => {
-        console.log(err)
-      })
-      this.frameHandler = window.requestAnimationFrame(() => this.scaning(current))
+      this.worker.postMessage({ type: 'scan', data: img })
+
+      // readBarcodesFromImageData(img, {
+      //   formats: this.formats,
+      //   // TODO: 当一次识别出多个时，允许选择
+      //   maxNumberOfSymbols: 1,
+      // }).then(res => {
+      //   if (Array.isArray(res) ? res.length === 0 : res.scanType === 'None') return
+
+      //   this.emitRes(res)
+      //   uni.navigateBack()
+      // }).catch(err => {
+      //   console.log(err)
+      // })
+
+      // this.stats.end()
     },
     onUnload() {
       window.cancelAnimationFrame(this.frameHandler)
@@ -186,6 +256,7 @@ export default {
     emitRes(res) {
       const _res = Array.isArray(res) ? res[0] : res
       this.event.emit('onScanRes', normalizeResult(_res))
+      uni.navigateBack()
     },
     handleClose() {
       uni.navigateBack()
@@ -195,26 +266,27 @@ export default {
         count: 1,
         sourceType: ['album'],
         success: async (res) => {
-          const _res = await this.readFromAlbum(res.tempFiles[0])
-          if (Array.isArray(_res) ? _res.length === 0 : _res.scanType === 'None') {
-            uni.showToast({
-              title: '识别失败',
-              icon: 'error',
-            })
-          } else {
-            this.emitRes(_res)
-            uni.navigateBack()
-          }
+          this.readFromAlbum(res.tempFiles[0])
+          // if (Array.isArray(_res) ? _res.length === 0 : _res.scanType === 'None') {
+          //   uni.showToast({
+          //     title: '识别失败',
+          //     icon: 'error',
+          //   })
+          // } else {
+          //   this.emitRes(_res)
+          //   uni.navigateBack()
+          // }
         }
       })
     },
     async readFromAlbum(file) {
-      const res = await readBarcodesFromImageFile(file, {
-        formats: this.formats,
-        // TODO: 当一次识别出多个时，允许选择
-        maxNumberOfSymbols: 1,
-      })
-      return res
+      this.worker.postMessage({ type: 'file', data: file })
+      // const res = await readBarcodesFromImageFile(file, {
+      //   formats: this.formats,
+      //   // TODO: 当一次识别出多个时，允许选择
+      //   maxNumberOfSymbols: 1,
+      // })
+      // return res
     }
   }
 }
